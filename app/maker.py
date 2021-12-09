7c906df45f34f2adf36dd67202d004898afb4338
Amir Dashti <javaher21@gmail.com>
2021-12-09 13:25:07 +0100

    base code for bug report analysis

--- app/maker.py
+++ app/maker.py
@@ -1,24 +1,43 @@
 import mysql.connector
+import pandas as pd
+
+# select which project to check
+def project_selector():
+    import inquirer
+    questions = [
+        inquirer.List('project',
+                      message="What size do you need?",
+                      choices=['jdt', 'swt', 'birt', 'eclipse_platform_ui'],
+                      ),
+    ]
+    answers = inquirer.prompt(questions)
+    return answers['project']
+
+
+# connect to a selected database
+def mysql_connection(project_name):
+    return mysql.connector.connect(
+        host='localhost',
+        port='32000',
+        user='root',
+        password='root',
+        database=project_name
+    )
 
 
 print('Running Python')
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  port="",
-  user="yourusername",
-  password="yourpassword",
-  database="mydatabase"
-)
+project = project_selector()
+database = mysql_connection(project)
 
-mycursor = mydb.cursor()
+# get all bugs
+builder = database.cursor()
+builder.execute("SELECT * FROM bug_and_files ORDER BY report_time")
+result = pd.DataFrame(builder.fetchall())
+result.columns = builder.column_names
 
-mycursor.execute("SELECT * FROM customers")
+# loop through each bug
+for index, row in result.iterrows():
+    print(row['bug_id'])
 
-myresult = mycursor.fetchall()
-
-for x in myresult:
-    print(x)
-
-
-
+database.close()
