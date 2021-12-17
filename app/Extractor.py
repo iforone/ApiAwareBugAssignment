f90981204fdff8c2761288695bca3e6519fa3bb5
Amir Dashti <javaher21@gmail.com>
2021-12-17 09:02:59 +0100

    cleanup

--- app/Extractor.py
+++ app/Extractor.py
@@ -6,9 +6,6 @@ def get_imports(file):
     file = open(file, mode='r')
     text = file.read()
     file.close()
-    print(text)
-    #imports = re.findall(r'import .*(?!org.eclipse.jdt).*;', text)
-    #imports = re.findall(r'import .*((?!org.eclipse.jdt).)*.+;', text)
     imports = re.findall(r'import (?!org.eclipse|\\.).*;', text)
     exit(imports)
 
