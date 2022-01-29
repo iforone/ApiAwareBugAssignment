102f4a1d7e0bef7d47b3b365846c8b6c29453d60
Amir Dashti <javaher21@gmail.com>
2022-01-29 20:20:57 +0100

    Profile complete

--- app/Profile.py
+++ app/Profile.py
@@ -9,6 +9,19 @@ def array_to_frequency_list(words, date):
     return list_
 
 
+def frequency_to_frequency_list(word_str_, date):
+    if word_str_ is None or word_str_ == '':
+        return {}
+
+    list_ = {}
+    words = word_str_.split(',')
+    for word in words:
+        sp = word.split(':')
+        list_[sp[0]] = {'frequency': sp[1], 'date': date}
+
+    return list_
+
+
 class Profile:
     def __init__(self, name, history, code, api):
         self.name = name
@@ -16,12 +29,33 @@ class Profile:
         self.code = code
         self.api = api
 
+    def __getitem__(self, name):
+        return getattr(self, name)
+
+    def __setitem__(self, name, value):
+        return setattr(self, name, value)
+
+    def __delitem__(self, name):
+        return delattr(self, name)
+
+    def __contains__(self, name):
+        return hasattr(self, name)
+
     def update_history(self, list_):
+        self.update(list_, 'history')
+
+    def update_code(self, list_):
+        self.update(list_, 'code')
+
+    def update_api(self, list_):
+        self.update(list_, 'api')
+
+    def update(self, list_, key):
         for i, content in list_.items():
-            if i in self.history:
-                self.history[i]['frequency'] += content['frequency']
+            if i in self[key]:
+                self[key][i]['frequency'] += content['frequency']
                 # update date only if it is more in the future
-                if self.history[i]['date'] < content['date']:
-                    self.history[i]['date'] = content['date']
+                if self[key][i]['date'] < content['date']:
+                    self[key][i]['date'] = content['date']
             else:
-                self.history[i] = content
+                self[key][i] = content
